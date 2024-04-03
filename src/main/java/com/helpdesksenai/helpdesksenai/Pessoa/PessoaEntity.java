2603b264f06fb0f7f855b30fe2c9426533e39129
Alice Medeiros da Silva <alice_silva4@estudante.sesisenai.org.br>
2024-04-02 21:56:49 -0300

    Commit Entity

---
+++ src/main/java/com/helpdesksenai/helpdesksenai/Pessoa/PessoaEntity.java
@@ -0,0 +1,97 @@
+package com.helpdesksenai.helpdesksenai.Pessoa;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+public class PessoaEntity implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    protected Integer id;
+    protected String nome;
+    @Column(unique = true)
+    protected String cpf;
+    @Column(unique = true)
+    protected String email;
+    protected String senha;
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "PERFIS")
+    protected Set<Integer> perfis = new HashSet<>();
+    @JsonFormat(pattern ="dd/MM/yyyy")
+    protected LocalDate dataCriacao = LocalDate.now();
+
+    public PessoaEntity(Integer id, String nome, String cpf, String email, String senha) {
+        this.id = id;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+        this.senha = senha;
+    }
+
+    public PessoaEntity() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getCpf() {
+        return cpf;
+    }
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getSenha() {
+        return senha;
+    }
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    public Set<Integer> getPerfis() {
+        return perfis;
+    }
+    public void setPerfis(Set<Integer> perfis) {
+        this.perfis = perfis;
+    }
+    public LocalDate getDataCriacao() {
+        return dataCriacao;
+    }
+    public void setDataCriacao(LocalDate dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PessoaEntity that = (PessoaEntity) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
